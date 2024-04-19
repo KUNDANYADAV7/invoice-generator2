e6a11a462c2d0fc8d2348346ec8bb64581740d86
KUNDANYADAV7 <kdyd999@gmail.com>
2024-04-19 14:14:47 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-// import React from 'react';
-// import './App.css';
-// import InvoiceGenerator from './Invoice';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <InvoiceGenerator />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from "react";
 import "./App.css";
 import InvoiceGenerator from "./Invoice";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
